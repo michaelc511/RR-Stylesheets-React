89c64489f9a40963834a62002d78fe93d0a69075
Michael Chang <michaelc511@gmail.com>
2022-08-27 11:49:25 -0700

    fs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Example from "./Example";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Example />
     </div>
   );
 }
